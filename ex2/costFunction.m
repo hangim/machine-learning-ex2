af7537e3ff6df1dadb67240d31d692d65d9065a5
hang <git@hang.im>
2015-10-20 21:08:26 +0800

    Logistic Regression Cost and Gradient

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -19,14 +19,11 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+H = sigmoid(X * theta);
 
+J = sum(-y .* log(H) - (1 - y) .* log(1 - H)) / m;
 
-
-
-
-
-
-
+grad = sum((H - y) .* X) / m;
 % =============================================================
 
 end
