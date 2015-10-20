08ca5c0bbee2893d34e77f792b52ae2e2cce229d
hang <git@hang.im>
2015-10-20 20:48:41 +0800

    Sigmoid Function

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -8,7 +8,7 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
+g = 1 ./ (1 + exp(-z));
 
 
 
