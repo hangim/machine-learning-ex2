db2a1c52aaa9e7daf9d3815ca6b69c08c070c556
hang <git@hang.im>
2015-10-20 20:42:34 +0800

    visualizing the data

--- ex2/plotData.m
+++ ex2/plotData.m
@@ -11,11 +11,11 @@ figure; hold on;
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
 %
+pos = find(y == 1);
+neg = find(y == 0);
 
-
-
-
-
+plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'LineWidth', 2, 'MarkerSize', 7);
 
 
 
